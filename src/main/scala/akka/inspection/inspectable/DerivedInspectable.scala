fb00df3efcafc43beb4562f061714c29d1415963
Dennis van der Bij <dennis@swissborg.com>
2019-03-04 20:46:11 +0100

    smarter derivation

--- src/main/scala/akka/inspection/inspectable/DerivedInspectable.scala
+++ src/main/scala/akka/inspection/inspectable/DerivedInspectable.scala
@@ -4,14 +4,34 @@ import akka.inspection
 import akka.inspection.ActorInspection
 import akka.inspection.ActorInspection.FragmentId
 import akka.inspection.Fragment._
+import akka.inspection.inspectable.DerivedInspectable.{Bar, Baz}
+import akka.inspection.util.Render
 import cats.implicits._
-import cats.{Always => _, _}
-import shapeless.{::, Cached, Generic, HList, HNil, Lazy, Strict}
+import cats.{Always => _}
+import shapeless.labelled.FieldType
+import shapeless.{::, Cached, HList, HNil, LabelledGeneric, Lazy, Strict, Witness}
 
 sealed trait DerivedInspectable[A] extends Inspectable[A]
 
+object Bla extends App {
+//  implicit def a[A]: Render[A] = _.toString
+//  implicit val l: Render[List[Int]] = _.toString
+
+  val baz: Inspectable[Baz] = DerivedInspectable.gen
+  println(baz.fragments)
+  println(baz.fragments(FragmentId("bar")).run(Baz(Bar(5, List(1, 2, 3)))))
+}
+
 object DerivedInspectable {
-  def gen[A, R](implicit G: Generic.Aux[A, R], R: Cached[Strict[DerivedInspectable[R]]]): DerivedInspectable[A] =
+  case class Bla()
+  case class Bar(i: Int, l: List[Int])
+  case class Baz(bar: Bar)
+
+  val bla: Inspectable[Bla] = DerivedInspectable.gen
+  val bar: Inspectable[Bar] = DerivedInspectable.gen
+
+  def gen[A, R](implicit G: LabelledGeneric.Aux[A, R],
+                R: Cached[Strict[DerivedInspectable[R]]]): DerivedInspectable[A] =
     new DerivedInspectable[A] {
       override def fragments: Map[ActorInspection.FragmentId, inspection.Fragment[A]] =
         R.value.value.fragments.mapValues {
@@ -22,50 +42,17 @@ object DerivedInspectable {
         }
     }
 
-  implicit def hcons[H, T <: HList](implicit H: Lazy[Inspectable[H]],
-                                    T: DerivedInspectable[T]): DerivedInspectable[H :: T] =
-    new DerivedInspectable[H :: T] {
-      override def fragments: Map[ActorInspection.FragmentId, inspection.Fragment[H :: T]] = {
-        val fragmentsH = H.value.fragments
-        val fragmentsT = T.fragments
-
-        fragmentsH.foldLeft(Map.empty[FragmentId, inspection.Fragment[H :: T]]) {
-          case (fragmentsHT, (id, fragmentH)) =>
-            fragmentsHT + (id -> fragmentsT
-              .get(id)
-              .fold[inspection.Fragment[H :: T]](fragmentH.contramap(_.head))(
-                fragmentT =>
-                  ContravariantMonoidal[inspection.Fragment]
-                    .product(fragmentH, fragmentT)
-                    .contramap(hcons => (hcons.head, hcons.tail))
-              ))
-        }
-      }
+  implicit def hcons[K <: Symbol, H, T <: HList](implicit K: Witness.Aux[K],
+                                                 H: Lazy[Render[FieldType[K, H]]],
+                                                 T: DerivedInspectable[T]): DerivedInspectable[FieldType[K, H] :: T] =
+    new DerivedInspectable[FieldType[K, H] :: T] {
+      override def fragments: Map[ActorInspection.FragmentId, inspection.Fragment[FieldType[K, H] :: T]] =
+        T.fragments.mapValues(
+          _.contramap[FieldType[K, H] :: T](_.tail)
+        ) + (FragmentId(K.value.name) -> Fragment.state(_.head)(H.value))
     }
 
   implicit val hnil: DerivedInspectable[HNil] = new DerivedInspectable[HNil] {
     override def fragments: Map[FragmentId, inspection.Fragment[HNil]] = Map.empty
   }
-
-//  implicit def ccons[H, T <: Coproduct](implicit H: Lazy[Inspectable[H]],
-//                                        T: DerivedInspectable[T]): DerivedInspectable[H :+: T] = ???
-//    new DerivedInspectable[H :+: T] {
-//      override def fragments: Map[FragmentId, inspection.Fragment[H :+: T]] = {
-//        val fragmentsH = Inspectable[H].fragments
-//        val fragmentsT = Inspectable[T].fragments
-//
-//        fragmentsH.foldLeft(Map.empty[FragmentId, inspection.Fragment[H :+: T]]) {
-//          case (fragmentsHT, (id, fragmentH)) =>
-//            val f: inspection.Fragment[H :+: T] = fragmentH
-//            ???
-//        }
-//
-//        ???
-//      }
-//    }
-
-//  implicit val cnil: DerivedInspectable[CNil] = ???
-//    override def fragments: Map[FragmentId, inspection.Fragment[CNil]] = Map.empty
-//  }
-
 }

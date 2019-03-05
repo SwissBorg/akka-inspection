90c6829edef48cf4bbb6c2a7cadfceabf9642f57
Dennis van der Bij <dennis@swissborg.com>
2019-03-05 20:31:23 +0100

    add inspectable derivation for nested product types

--- src/main/scala/akka/inspection/inspectable/DerivedInspectable.scala
+++ src/main/scala/akka/inspection/inspectable/DerivedInspectable.scala
@@ -13,30 +13,38 @@ import shapeless.{::, Cached, HList, HNil, LabelledGeneric, Lazy, Strict, Witnes
 sealed trait DerivedInspectable[A] extends Inspectable[A]
 
 object DerivedInspectable extends LowPriorityDerivedInspectable {
-  def gen[A, R](implicit G: LabelledGeneric.Aux[A, R],
-                R: Cached[Strict[DerivedInspectable[R]]]): DerivedInspectable[A] =
+
+  /**
+   * Derive an `Inspectable[A]`.
+   */
+  def gen[A, Repr](implicit gen: LabelledGeneric.Aux[A, Repr],
+                   inspectableRepr: Cached[Strict[DerivedInspectable[Repr]]]): DerivedInspectable[A] =
     new DerivedInspectable[A] {
       override def fragments: Map[ActorInspection.FragmentId, inspection.Fragment[A]] =
-        R.value.value.fragments.mapValues {
+        inspectableRepr.value.value.fragments.mapValues {
           case Fix(fragment)    => Fix(fragment)
           case Always(fragment) => Always(fragment)
-          case Given(fragment)  => Given(fragment.compose(G.to))
+          case Given(fragment)  => Given(fragment.compose(gen.to))
           case Undefined()      => Undefined()
         }
     }
 
-  implicit def hconsDerivedInspectable0[K <: Symbol, H, T <: HList](
-    implicit K: Witness.Aux[K],
-    H: Lazy[Render[FieldType[K, H]]],
-    T: DerivedInspectable[T]
-  ): DerivedInspectable[FieldType[K, H] :: T] =
-    new DerivedInspectable[FieldType[K, H] :: T] {
-      override def fragments: Map[ActorInspection.FragmentId, inspection.Fragment[FieldType[K, H] :: T]] =
-        // TODO MAP!!
-        T.fragments.mapValues(
-          _.contramap[FieldType[K, H] :: T](_.tail)
-        ) + (FragmentId(K.value.name) -> Fragment.state(_.head)(H.value))
+  implicit def hconsDerivedInspectable0[K <: Symbol, H, Repr <: HList, T <: HList](
+    implicit witness: Witness.Aux[K],
+    gen: LabelledGeneric.Aux[H, Repr],
+    derivedInspectableR: Lazy[DerivedInspectable[Repr]],
+    derivedInspectableT: DerivedInspectable[T]
+  ): DerivedInspectable[FieldType[K, H] :: T] = new DerivedInspectable[FieldType[K, H] :: T] {
+    override def fragments: Map[FragmentId, inspection.Fragment[FieldType[K, H] :: T]] = {
+      val fragmentsR = derivedInspectableR.value.fragments.map {
+        case (FragmentId(id), fragment) =>
+          (FragmentId(s"${witness.value.name}.$id"),
+           fragment.contramap[FieldType[K, H] :: T](hcons => gen.to(hcons.head)))
+      }
+
+      derivedInspectableT.fragments.mapValues(_.contramap[FieldType[K, H] :: T](_.tail)) ++ fragmentsR
     }
+  }
 
   implicit val hnilDerivedInspectable: DerivedInspectable[HNil] = new DerivedInspectable[HNil] {
     override def fragments: Map[FragmentId, inspection.Fragment[HNil]] = Map.empty
@@ -44,21 +52,18 @@ object DerivedInspectable extends LowPriorityDerivedInspectable {
 }
 
 trait LowPriorityDerivedInspectable {
-  // TODO nested doesn't work :/ never used
-  implicit def hconsDerivedInspectable1[K <: Symbol, H, R <: HList, T <: HList](
-    implicit K: Witness.Aux[K],
-    G: LabelledGeneric.Aux[H, R],
-    R: Lazy[DerivedInspectable[R]],
-    T: DerivedInspectable[T]
-  ): Inspectable[FieldType[K, H] :: T] = new Inspectable[FieldType[K, H] :: T] {
-    override def fragments: Map[FragmentId, inspection.Fragment[FieldType[K, H] :: T]] = {
-      val fragmentsR = R.value.fragments.map {
-        case (FragmentId(id), fragment) =>
-          (FragmentId(s"${K.value.name}.$id"), fragment.contramap[FieldType[K, H] :: T](hcons => G.to(hcons.head)))
-      }
-
-      T.fragments.mapValues(_.contramap[FieldType[K, H] :: T](_.tail)) ++ fragmentsR
+  implicit def hconsDerivedInspectable1[K <: Symbol, H, T <: HList](
+    implicit witness: Witness.Aux[K],
+    renderH: Lazy[Render[FieldType[K, H]]],
+    inspectableT: DerivedInspectable[T]
+  ): DerivedInspectable[FieldType[K, H] :: T] =
+    new DerivedInspectable[FieldType[K, H] :: T] {
+      override def fragments: Map[ActorInspection.FragmentId, inspection.Fragment[FieldType[K, H] :: T]] =
+        inspectableT.fragments.map {
+          case (id, fragment) =>
+            (id, fragment.contramap[FieldType[K, H] :: T](_.tail))
+        } + (FragmentId(
+          witness.value.name
+        ) -> Fragment.state(_.head)(renderH.value))
     }
-  }
-
 }
